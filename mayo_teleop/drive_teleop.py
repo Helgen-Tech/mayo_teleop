a978663c71adebdee2b000c2ebf406cd023171e6
Jeza <142040263+Jez4SM@users.noreply.github.com>
2023-11-30 17:56:31 -0600

    Jeza_update

--- mayo_teleop/drive_teleop.py
+++ mayo_teleop/drive_teleop.py
@@ -1,89 +1,265 @@
 import rclpy
-# import the ROS2 python libraries
+import threading
 from rclpy.node import Node
-# import the Twist interface from the geometry_msgs package
-from geometry_msgs.msg import Twist
+import numpy as np
 from sensor_msgs.msg import Joy
 from sensor_msgs.msg import JointState
 
 from rclpy.qos import ReliabilityPolicy, QoSProfile
 
-
 class DriveTeleop(Node):
 
     def __init__(self):
-        # Here you have the class constructor
-        # call super() in the constructor to initialize the Node object
-        # the parameter you pass is the node name
         super().__init__('drive_teleop')
-        # create the publisher object
-        # in this case, the publisher will publish on /cmd_vel Topic with a queue size of 10 messages.
-        # use the Twist module for /cmd_vel Topic
-        self.cmd_publisher_ = self.create_publisher(Twist, 'cmd_vel', 10)
         self.joint_publisher_ = self.create_publisher(JointState, 'joint_command', 10)
-        self.subscriber = self.create_subscription(Joy, '/joy', self.joy_callback, QoSProfile(depth=10, reliability=ReliabilityPolicy.BEST_EFFORT))
+        self.joint_publisher_1 = self.create_publisher(JointState, 'joint_command_vel', 10)
+        self.subscriber_2 = self.create_subscription(Joy, '/joy1', self.handle_keyboard_input, QoSProfile(depth=10, reliability=ReliabilityPolicy.BEST_EFFORT))
 
-        # cmd_vel command
-        self.speed_setting = 2 # default to medium speed
+        #thread
+        self.lock = threading.Lock()
+        self.input_thread = threading.Thread(target=self.handle_keyboard_input)
+        self.input_thread.daemon = True
+        self.input_thread.start()
+        
+
+        # cmd_vel_nav command
+        self.speed_setting = 2 # default to medium speed 2
         self.linear_x_speed = 0
         self.linear_y_speed = 0
         self.angular_z_speed = 0
 
+
         # Fork positioner prism
         self.lifting_mechanism_speed = 1
-        self.lifting_mechanism_min = 0
-        self.lifting_mechanism_max = 50
-        self.lifting_mechanism_position = 0.0
+        self.lifting_mechanism_min = -10.0
+        self.lifting_mechanism_max = 40.0
+        self.lifting_mechanism_position = -10.0
 
         # Right fork prism & left fork prism
         self.fork_positioner_speed = 1
-        self.fork_positioner_min = 0
-        self.fork_positioner_max = 30
+        self.fork_positioner_min = -20
+        self.fork_positioner_max = 5
         self.fork_positioner_position = 0.0
 
-    def joy_callback(self,msg):
-        # Set speed ratio using d-pad
-        if msg.axes[7] == 1: # full speed (d-pad up)
-            self.speed_setting = 1
-        if msg.axes[6] != 0: # medium speed (d-pad left or right)
-            self.speed_setting = 2
-        if msg.axes[7] == -1: # low speed (d-pad down)
-            self.speed_setting = 3
+        # Tilting prism
+        self.tilt_speed = 1
+        self.tilt_min = -5.0
+        self.tilt_max = 5.0
+        self.tilt_position = 0.0
 
-        # Lifting mechanism control 
-        if msg.buttons[0]: # X, go down mechanism
-            if self.lifting_mechanism_position < self.lifting_mechanism_max:
-                self.lifting_mechanism_position += self.lifting_mechanism_speed
-        if msg.buttons[2]: # Triangle, go up mechanism
-            if self.lifting_mechanism_position > self.lifting_mechanism_min:
-                self.lifting_mechanism_position -= self.lifting_mechanism_speed
+        #Mecanum Angles Doubles 
+        # make an array 
+        self.wheel_0_ang=np.pi/4
+        self.wheel_1_ang=-np.pi/4
+        self.wheel_2_ang=-np.pi/4
+        self.wheel_3_ang=np.pi/4
 
-        # Left Fork positioner control
-        if msg.buttons[3]: # Square, close forks
-            if self.fork_positioner_position < self.fork_positioner_max:
-                self.fork_positioner_position += self.fork_positioner_speed
-        if msg.buttons[1]: # Circle, open forks
-            if self.fork_positioner_position > self.lifting_mechanism_min:
-                self.fork_positioner_position -= self.fork_positioner_speed
+        #Velocities for each Wheel
+        #remove twist -> make vector 
+        self.speed_wheel0=0.0 #LR Inferior Izquierda
+        self.speed_wheel1=0.0 #RR Inferior derecha
+        self.speed_wheel2=0.0#RF Delantera derecha
+        self.speed_wheel3=0.0#LF Delantera izquierda
+        
+        #Wheel Orientations
+        self.wheelO_0=np.array([1.0,0.0])
+        self.wheelO_1=np.array([1.0,0.0])
+        self.wheelO_2=np.array([1.0,0.0])
+        self.wheelO_3=np.array([1.0,0.0])
 
-        # Drive sticks
-        self.linear_x_speed = msg.axes[1] / self.speed_setting # left stick up/down
-        self.linear_y_speed = msg.axes[0] / self.speed_setting # left stick left/right
-        self.angular_z_speed = -msg.axes[3] / self.speed_setting # right stick left/right
+        #Wheel Positions
+        self.wheelP_0=np.array([0.71721,0.555])
+        self.wheelP_1=np.array([0.71721,-0.555])
+        self.wheelP_2=np.array([-0.71721,0.555])
+        self.wheelP_3=np.array([-0.71721,-0.555])
+        self.wheel_l1=0.71721
+        self.wheel_l2=0.555
 
-        # joint_state
-        joint_state_position = JointState()
-        joint_state_position.name = ["fork_positioner_prism", "right_fork_prism", "left_fork_prism"]
-        joint_state_position.position = [self.lifting_mechanism_position/100, self.fork_positioner_position/100, -self.fork_positioner_position/100]
-        self.joint_publisher_.publish(joint_state_position)
+        #Wheel Radious
+        #Same size for 4 
+        self.wheels_rad=0.2
 
-        # cmd_vel
-        twist_msg = Twist()
-        twist_msg.linear.x = self.linear_x_speed
-        twist_msg.linear.y = self.linear_y_speed
-        twist_msg.angular.z = self.angular_z_speed
-        self.cmd_publisher_.publish(twist_msg)
-        self.get_logger().info('Publishing: "%s"' % twist_msg)
+    def kinematic_vel(self, Vx, Vy, omega):
+        self.omega_lf = 1 / self.wheels_rad * (Vx - Vy - (self.wheel_l1 + self.wheel_l2) * omega)
+        self.omega_rf = 1 / self.wheels_rad * (Vx + Vy + (self.wheel_l1 + self.wheel_l2) * omega)
+        self.omega_lr = 1 / self.wheels_rad * (Vx + Vy - (self.wheel_l1 + self.wheel_l2) * omega)
+        self.omega_rr = 1 / self.wheels_rad * (Vx - Vy + (self.wheel_l1 + self.wheel_l2) * omega)
+        
+        
+        return self.omega_lr, self.omega_rr, self.omega_rf, self.omega_lf
+
+
+    def handle_keyboard_input(self):
+        while rclpy.ok():
+            with self.lock:
+                # Set speed ratio using 1,2,3
+                adjust_speed_input = input("op: ")
+                if adjust_speed_input == '1':
+                    self.speed_setting = 0.5
+                    print("Low speed.")
+                elif adjust_speed_input == '2':
+                    self.speed_setting = 1
+                    print("medium speed.")
+                elif adjust_speed_input == '3':
+                    self.speed_setting = 2
+                    print("High speed.")
+                    ##If the speed exceeds 3, there may be a phase shift in the velocities.
+                                
+                #Velocity commands
+                # Lifting mechanism control
+                if 'w' in adjust_speed_input:
+                    for _ in range(20):  
+                        if self.lifting_mechanism_position < self.lifting_mechanism_max:
+                            self.lifting_mechanism_position += self.lifting_mechanism_speed
+                    print("Lifting complete.")
+                elif 's' in adjust_speed_input:
+                    for _ in range(20):  
+                        if self.lifting_mechanism_position > self.lifting_mechanism_min:
+                            self.lifting_mechanism_position -= self.lifting_mechanism_speed
+                    print("Lowering complete.")
+                
+                # Left Fork positioner control
+                if 'd' in adjust_speed_input:
+                    for _ in range(20):  
+                        if self.fork_positioner_position < self.fork_positioner_max:
+                            self.fork_positioner_position += self.fork_positioner_speed
+                    print("Fork positioner moving to the right.")
+                elif 'a' in adjust_speed_input:
+                    for _ in range(20):  
+                        if self.fork_positioner_position > self.lifting_mechanism_min:
+                            self.fork_positioner_position -= self.fork_positioner_speed
+                    print("Fork positioner moving to the left.")
+
+                #tilt
+                if adjust_speed_input == 't':
+                    self.tilt_position = self.tilt_min
+                    print("Tilting.")
+                elif adjust_speed_input == 'r':
+                    self.tilt_position = 0.0
+                    print("Resetting tilt.")
+
+                #Velocity
+                if adjust_speed_input == 'y':
+                    #All positive move foward x
+                    for _ in range(20):
+                        self.linear_x_speed = 0.5 *self.speed_setting
+                        self.linear_y_speed = 0*self.speed_setting
+                        self.angular_z_speed = 0*self.speed_setting
+                        self.speed_wheel0, self.speed_wheel1, self.speed_wheel2, self.speed_wheel3 = self.kinematic_vel(self.linear_x_speed, self.linear_y_speed, self.angular_z_speed)
+                    print('velocidad rueda')
+                    print(self.speed_wheel0)
+                    #Calcular velocidad de las ruedas con la funcion
+                    
+                if adjust_speed_input == 'u':
+                    #All negative move backward -x ok
+                    for _ in range(20):
+                    
+                        self.linear_x_speed = -0.5 *self.speed_setting
+                        self.linear_y_speed = 0.0*self.speed_setting
+                        self.angular_z_speed = 0.0*self.speed_setting
+                        self.speed_wheel0, self.speed_wheel1, self.speed_wheel2, self.speed_wheel3 = self.kinematic_vel(self.linear_x_speed, self.linear_y_speed, self.angular_z_speed)
+
+                    
+                if adjust_speed_input == 'j':
+                    #move left positive 
+                    for _ in range(20):
+                        self.linear_x_speed = 0.0 *self.speed_setting
+                        self.linear_y_speed = 0.5*self.speed_setting
+                        self.angular_z_speed = 0.0*self.speed_setting
+                        self.speed_wheel0, self.speed_wheel1, self.speed_wheel2, self.speed_wheel3 = self.kinematic_vel(self.linear_x_speed, self.linear_y_speed, self.angular_z_speed)
+                    
+                if adjust_speed_input == 'k':
+                    #move left negative 
+                    for _ in range(20):
+                        self.linear_x_speed = 0.0 *self.speed_setting
+                        self.linear_y_speed = -0.5*self.speed_setting
+                        self.angular_z_speed = 0.0*self.speed_setting
+                        self.speed_wheel0, self.speed_wheel1, self.speed_wheel2, self.speed_wheel3 = self.kinematic_vel(self.linear_x_speed, self.linear_y_speed, self.angular_z_speed)
+
+                    
+                if adjust_speed_input == 'l':
+                    #right diagonal foward
+                    for _ in range(20):
+                        self.linear_x_speed = 0.5 *self.speed_setting
+                        self.linear_y_speed = 0.5*self.speed_setting
+                        self.angular_z_speed = 0.0*self.speed_setting
+                        self.speed_wheel0,self.speed_wheel1, self.speed_wheel2,self.speed_wheel3 = self.kinematic_vel(self.linear_x_speed, self.linear_y_speed, self.angular_z_speed)
+                        self.speed_wheel1=0.0
+                        self.speed_wheel3=0.0
+                if adjust_speed_input == 'm':
+                    #rigth diogonal backwards
+                    for _ in range(20):
+                        self.linear_x_speed = -0.5 *self.speed_setting
+                        self.linear_y_speed = 0.5*self.speed_setting
+                        self.angular_z_speed = 0.0*self.speed_setting
+                        self.speed_wheel0,self.speed_wheel1, self.speed_wheel2,self.speed_wheel3 = self.kinematic_vel(self.linear_x_speed, self.linear_y_speed, self.angular_z_speed)
+                        self.speed_wheel2=0.0
+                        self.speed_wheel0=0.0
+                if adjust_speed_input == 'v':
+                    #move right  diagonal foward
+                    for _ in range(20):
+                        self.linear_x_speed = 0.5 *self.speed_setting
+                        self.linear_y_speed = -0.5*self.speed_setting
+                        self.angular_z_speed = 0.0*self.speed_setting
+                        self.speed_wheel0,self.speed_wheel1, self.speed_wheel2,self.speed_wheel3 = self.kinematic_vel(self.linear_x_speed, self.linear_y_speed, self.angular_z_speed)
+                        self.speed_wheel2=0.0
+                        self.speed_wheel0=0.0
+                if adjust_speed_input == 'b':
+                    #left  diagonal backward
+                    for _ in range(20):
+                        self.linear_x_speed = -0.5 *self.speed_setting
+                        self.linear_y_speed = -0.5*self.speed_setting
+                        self.angular_z_speed = 0.0*self.speed_setting
+                        self.speed_wheel0,self.speed_wheel1, self.speed_wheel2,self.speed_wheel3 = self.kinematic_vel(self.linear_x_speed, self.linear_y_speed, self.angular_z_speed)
+                        self.speed_wheel1=0.0
+                        self.speed_wheel3=0.0
+
+                if adjust_speed_input == 'z':
+                    #Stop
+                    for _ in range(20):
+                        self.linear_x_speed = 0.0 *self.speed_setting
+                        self.linear_y_speed = 0.0*self.speed_setting
+                        self.angular_z_speed = 0.0*self.speed_setting
+                        self.speed_wheel0, self.speed_wheel1, self.speed_wheel2, self.speed_wheel3 = self.kinematic_vel(self.linear_x_speed, self.linear_y_speed, self.angular_z_speed)
+                if adjust_speed_input == 'i':
+                    # Move in his own axis +z
+                    for _ in range(20):    
+                        self.linear_x_speed = 0.0 *self.speed_setting
+                        self.linear_y_speed = 0.0*self.speed_setting
+                        self.angular_z_speed= 0.5*self.speed_setting
+                        self.speed_wheel0, self.speed_wheel1, self.speed_wheel2, self.speed_wheel3 = self.kinematic_vel(self.linear_x_speed, self.linear_y_speed, self.angular_z_speed)
+                        self.speed_wheel0=0.0 
+                        self.speed_wheel1=0.0
+
+                  
+                    
+                if adjust_speed_input == 'h':
+                    #Move in his own axis -z negative
+                    for _ in range(20):
+                        self.linear_x_speed = 0.0 *self.speed_setting
+                        self.linear_y_speed = 0.0*self.speed_setting
+                        self.angular_z_speed = -0.5*self.speed_setting
+                        
+                        self.speed_wheel0, self.speed_wheel1, self.speed_wheel2, self.speed_wheel3 = self.kinematic_vel(self.linear_x_speed, self.linear_y_speed, self.angular_z_speed)
+                        self.speed_wheel2=0.0 
+                        self.speed_wheel3=0.0
+            # joint_state
+            joint_state_position = JointState()
+            joint_state_position.name = ["SliderLift", "SliderForkR", "SliderForkL", "SliderTiltL"]
+            joint_state_position.position = [self.lifting_mechanism_position/100, self.fork_positioner_position/100, self.fork_positioner_position/100, self.tilt_position/100]
+            self.joint_publisher_.publish(joint_state_position)
+       
+            #Wheels joint_state
+            joint_state_position_W = JointState()
+            joint_state_position_W.name = ["RevoluteWheelLR", "RevoluteWheelRR", "RevoluteWheelRF", "RevoluteWheelLF"]
+            joint_state_position_W.velocity = [self.speed_wheel0, self.speed_wheel1, self.speed_wheel2, self.speed_wheel3]
+            self.joint_publisher_1.publish(joint_state_position_W)
+
+            self.linear_x_speed=0.0
+            self.linear_y_speed=0.0
+            self.linear_y_speed=0.0
+
+    
             
 def main(args=None):
     # initialize the ROS communication
@@ -99,3 +275,30 @@ def main(args=None):
 
 if __name__ == '__main__':
     main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
